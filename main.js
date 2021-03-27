4149cf14228c92fa21215daacefa8eb0c16f9d6e
Octopus5W <piccoloero@piccoloero.localdomain>
2021-03-28 00:12:51 +0100

    ajout png + squelette html & css

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
-//imagge pokemon
-document.getElementById("infoPkmn").style.display= "flex";
-imgPokemon.src = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/"+pokedex+".png";
-function jsonAcces(){
+//récupération des données
+const container = document.getElementById('container');
+const container_pendu = document.getElementById('cont_img_pendu');
+const container_letters = document.getElementById('cont_img_pendu');
+let all_letters = document.getElementsByClassName('letters');
 
-    //Acces a mon fichier Json
-    //génaration d'un id random par apport au nombre d'id dans mon Json
-    //creation d'un objet avec le rnd généré
-        fetch("./pkmn(3G).json").then(res1 => res1.json())
-        .then(compter => {  wordLenght = (compter.length);
-                            rnd = Math.floor(Math.random() * wordLenght) 
-                            console.log("rnd = " + rnd)
 
-                            wordObj = compter.find(item => item.id == rnd);
 
-                            wordObj !== null ? console.log(wordObj) :
-                            console.log("Introuvable")})
-                            //console.log(wordObj)
-        .catch(document.querySelector('.state').innerText = "Connexion avec le dictionnaire Pokémon")
 
-        //Direction setTimeOut
+// //imagge pokemon
+// document.getElementById("infoPkmn").style.display= "flex";
+// imgPokemon.src = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/"+pokedex+".png";
+// function jsonAcces(){
+
+//     //Acces a mon fichier Json
+//     //génaration d'un id random par apport au nombre d'id dans mon Json
+//     //creation d'un objet avec le rnd généré
+//         fetch("./pkmn(3G).json").then(res1 => res1.json())
+//         .then(compter => {  wordLenght = (compter.length);
+//                             rnd = Math.floor(Math.random() * wordLenght) 
+//                             console.log("rnd = " + rnd)
+
+//                             wordObj = compter.find(item => item.id == rnd);
+
+//                             wordObj !== null ? console.log(wordObj) :
+//                             console.log("Introuvable")})
+//                             //console.log(wordObj)
+//         .catch(document.querySelector('.state').innerText = "Connexion avec le dictionnaire Pokémon")
+
+//         //Direction setTimeOut
                    
- }
+//  }
  
