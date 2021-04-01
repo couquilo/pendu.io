f5f6d8974e9ab0e761a9b11dfd44d3af4aa26c22
Octopus5W <piccoloero@piccoloero.localdomain>
2021-04-01 21:37:44 +0200

    css ok, except container_pendu

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const container_lettres = document.getElementById('container_lettres');
 const container_reponse = document.getElementById('container_reponse');
 const img_pendu = document.getElementById('img_pendu');
 const all_lettres = [...document.getElementsByClassName('lettres')];
-const reponse_input = document.getElementById('reponse');
+const reponse = document.getElementsByClassName('fin')[1]
 
 
 let name = prompt('quel mot doit il trouver')
@@ -21,7 +21,9 @@ all_lettres.map(elem => elem.addEventListener('click', iclicked));
 let lettres_trouvées = [];
 let nb_Erreur = 0;
 const nombre_de_vies = 10;
-actualisation_reponse_input()
+const false_color = '#aa2b1d';
+const true_color = '#cdc733';
+actualisation_reponse()
 
 
 function iclicked() {
@@ -29,11 +31,11 @@ function iclicked() {
         this.removeEventListener('click', iclicked)
         if (name[i].normalize("NFD").replace(/[\u0300-\u036f]/g, "") == this.innerText.toLowerCase()) {
             lettres_trouvées.push(name[i])
-            this.style.backgroundColor = "blue"
-            actualisation_reponse_input(this.innerText)
+            this.style.backgroundColor = true_color;
+            actualisation_reponse(this.innerText)
             break;
         }
-        this.style.backgroundColor = "red"
+        this.style.backgroundColor = false_color;
         if (i == name.length - 1) {
             console.log('ok')
             nb_Erreur += 1;
@@ -48,23 +50,21 @@ function iclicked() {
 function gagne(nb_Erreur) {
     retire_tous_event_click(all_lettres)
     container_reponse.innerHTML = `
-    <div id="gagne">Bien joué ! La réponse était <span id="user_word_choice">${name.join('')}<span>.</div>
-    <div id="gagne">Einstein a vécu ${nb_Erreur + 1} vies.</div>
-    <div id="gagne">En vivant seulement ${nb_Erreur} vie(s) tu deviens donc meilleur que lui ! </div>
+    <div class="fin">Bien joué ! La réponse était <span id="user_word_choice">${name.join('')}</span>.<br>
+        Einstein a vécu ${nb_Erreur + 1} vies.<br>
+        En vivant seulement ${nb_Erreur} vie(s) tu deviens donc meilleur que lui ! 
+    </div>
     `
 }
 
-// actualisation placeholder sur l'input
-function actualisation_reponse_input() {
-    reponse_input.placeholder = name.map(lettre => lettres_trouvées.find(lettre_trouvée => {
+// actualisation innerText sur l'input
+function actualisation_reponse() {
+    reponse.innerText = name.map(lettre => lettres_trouvées.find(lettre_trouvée => {
         if (lettre_trouvée == lettre)
             return lettre
     }) ? lettre : "*").join("");
-    console.log(name.join(''))
-    console.log(reponse_input.placeholde)
-    if (reponse_input.placeholder == name.join('')) {
+    if (reponse.innerText == name.join('')) {
         gagne(nb_Erreur);
-        console.log('onk')
     }
 }
 
@@ -72,8 +72,11 @@ function actualisation_reponse_input() {
 function perdu(nombre_de_vies) {
     retire_tous_event_click(all_lettres)
     container_reponse.innerHTML = `
-    <div id="gagne">Bon allé je t'aide. La réponse était <span>${name}<span>.</div>
-    <div id="perdu">${nombre_de_vies} vies ne t'auront donc pas suffit ?!</div>
+    <div class="fin">
+        ${nombre_de_vies} vies ne t'auront donc pas suffit ?!
+        <br>
+        Bon allé je t'aide. La réponse était <span id="user_word_choice">${name.join('')}</span>.
+    </div>
     `
 }
 
@@ -84,6 +87,6 @@ function retire_tous_event_click(all_lettres) {
 
 //actualisation de l'image du pendu
 function actualisation_img_pendu(nb_Erreur) {
-    img_pendu.src = `img/${nb_Erreur}.png`;
+    img_pendu.src = `img/${nb_Erreur}.svg` || `img/${nb_Erreur}.png`;
     console.log(img_pendu.src)
 }
